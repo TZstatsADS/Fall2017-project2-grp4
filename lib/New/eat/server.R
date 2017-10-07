55577cbbdd0f9541fd2b904c46a751870b0e1013
Fan Gong <gongfan1993@gmail.com>
2017-10-07 16:13:53 -0400

    new_version
    new

--- lib/New/eat/server.R
+++ lib/New/eat/server.R
@@ -1,25 +1,90 @@
 #FOR OUR GROUP
 
-options(shiny.maxRequestSize=50*1024^2)
-
-shinyServer(function(input, output, session) {
+shinyServer = function(input, output, session) {
   
-  
-  #generate data through input: result[1] = route, result[2] = ctb(to be debugged if only starting point)
-  Data<-reactive({
-    result<-list()
+  #####Create route dataframe
+  route_df <- reactive({
+    route_df <- route(input$from, input$to, structure = 'route', mode = 'transit',output = "simple")# transit
     
-    
-    if(!is.null(input$from) & !is.null(input$to)){
-      route_df <- route(input$startingPoint, input$destination, structure = 'route', mode = 'transit',output = "simple")# transit
-      
-      for (i in 1:dim(route_df)[1]){
-        route_df$address[i] = revgeocode(c(as.numeric(route_df$lon[i]),as.numeric(route_df$lat[i])))
-        route_df$url[i]=paste0("https://www.yelp.com/search?f&find_loc=",gsub('\\s',"+",  route_df$address[i]),"&ns=1",sep="")
-        route_df$content[i] = paste(sep = "<br/>",
-                                    "<b><a href=route_df$url[i]</a></b>",
-                                    route_df$address[i])
-      }
-      result[[1]] <- route_df
+    for (i in 1:dim(route_df)[1]){
+      route_df$address[i] = revgeocode(c(as.numeric(route_df$lon[i]),as.numeric(route_df$lat[i])))
+      route_df$url[i]=paste0("https://www.yelp.com/search?f&find_loc=",gsub('\\s',"+",  route_df$address[i]),"&ns=1",sep="")
+      route_df$content[i] = paste(sep = "<br/>",
+                                  "<b><a href=route_df$url[i]</a></b>",
+                                  route_df$address[i])
     }
+    return(route_df)
+  })
+  
+  
+  #####Create ctb dataframe that contains all the restaurant information 
+  ctb <- reactive({
+    res = POST("https://api.yelp.com/oauth2/token",
+               body = list(grant_type = "client_credentials",
+                           client_id = "KjjqVxt9pcpH6fWmEqVvEQ",
+                           client_secret = "ApFCYwJTIteL7mu146tmmLMuCui7unn2dcKN3ScFTrSLRLm8QWKNqkTWXtHz4OY1"))
+    token = content(res)$access_token
+    yelp = "https://api.yelp.com"
+    
+    # create an empty ctb dataframe
+    ctb = data.frame(matrix(NA, nrow = 0, ncol = 14))
+    
+    # a for loop for 4 locations in this case 
+    for (i in 1:nrow(route_df())){
+      url_i =modify_url(yelp, path = c("v3", "businesses", "search"),
+                        query = list(latitude =route_df()[i,'lat'],longitude = route_df()[i,'lon'],
+                                     open_now = T,
+                                     radius =round(1609* input$in_mile ,0)))
+      res_i = GET(url_i, add_headers('Authorization' = paste("bearer", token)))
+      #http_status(res)
+      ctb_list_i = content(res_i)$businesses
+      ctb_i=data.frame(matrix(NA, nrow = 0, ncol = 14))
+      for (j in 1:length(ctb_list_i)){ # create a J row dataframe
+        ctb_i[j,1] = ctb_list_i[[j]]$id
+        ctb_i[j,2] = ctb_list_i[[j]]$name
+        ctb_i[j,3] = ctb_list_i[[j]]$image_url
+        ctb_i[j,4] = ctb_list_i[[j]]$url
+        ctb_i[j,5] = ctb_list_i[[j]]$review_count
+        ctb_i[j,6] = ctb_list_i[[j]]$rating
+        ctb_i[j,7] = ifelse (is.null(ctb_list_i[[j]]$price),NA,ctb_list_i[[j]]$price)
+        ctb_i[j,8] = ctb_list_i[[j]]$display_phone
+        ctb_i[j,9] = ctb_list_i[[j]]$coordinates$latitude
+        ctb_i[j,10] = ctb_list_i[[j]]$coordinates$longitude
+        ctb_i[j,11] = ctb_list_i[[j]]$location$display_address[[1]]
+        ctb_i[j,12] = ctb_list_i[[j]]$location$zip_code
+        ctb_i[j,13] = ctb_list_i[[j]]$categories[[1]]$title
+        ctb_i[j,14] = i 
+        
+      }
+      ctb=rbind(ctb, ctb_i)
+    }
+    
+    colnames(ctb) = c("id","name","image_url","url","review_count","rating","price","display_phone","latitude","longitude","address","zip code","category","stop")
+    
+    ctb$price = factor(ctb$price)
+    return(ctb)
+  })
+  
+  #####Create Palette Variable
+  factpal = reactive({
+    return(colorFactor(rainbow(5), ctb()[,'price']))
+  })
+  
+  #####
+  route_df = reactive({
+    return(round(tapply(ctb()$rating,ctb()$stop,mean),2))
+  })
+  
+  #####Create map
+  
+  
+  
+  
+  #####
+  output$try = renderText({
+    print(route_df()$lat[1])
+  })
+  
+  
+}
     
--- lib/New/eat/ui.R
+++ lib/New/eat/ui.R
@@ -1,7 +1,16 @@
 #using 'shinydashboard' library
 
 library(shinydashboard)
-dbHeader<-dashboardHeader(title='Restaurants on your way', titleWidth = 350)
+library(ggmap)
+library(ggplot2)
+library(leaflet)
+library(htmltools)
+library(dplyr)
+library(plyr)
+library(httr)
+library(reshape2)
+library(purrr)
+
 
 dashboardPage(
   skin = "purple",
@@ -13,11 +22,11 @@ dashboardPage(
   dashboardSidebar(
     width = 310,
     textInput("from","Enter starting point:",'Time Square,NYC, NY, USA'),
-    textInput("to","Enter destination:",'Columbia University, NYC, NY, USA'),
+    textInput("to","Enter destination:",'Columbus Circle, NYC, NY, USA'),
     numericInput("in_mile","Max dist from your route (mi)",0.2, min = 0.1, max = 5),
     sliderInput("minStar","Minimum # of stars on Yelp",min = 1, max = 5, value = 1),
     sliderInput("price","Price Range",min = 1, max = 4, value = 1),
-    submitButton("Submit",width='100%')
+    submitButton("Submit",width='80%')
   ),
   
   dashboardBody(
@@ -29,8 +38,10 @@ dashboardPage(
       column(width=7,
              box(width = NULL, solidHeader = TRUE,
                  verbatimTextOutput("Click_text")),
-                 htmlOutput("image"))
-   )
+                 htmlOutput("image"))),
+    fluidRow(
+      column(width = 7, box(width = NULL,  textOutput('try')))
+    )
   )
 )
 
